080eae78308137704480dbf69dd4272db3e3e322
FaheemG22 <119604215+FaheemG22@users.noreply.github.com>
2023-11-10 23:16:45 +0000

    Database Test 2

--- test.php
+++ test.php
@@ -1,41 +1,43 @@
 <!DOCTYPE html>	
 <HTML>
 
-
-
-
 <?php
 $host = 'localhost';
 $S_user = 'root';
 $S_password = '';
 $db ='Coffee';
 
-$conn = mysqli_connect($host,$S_user,$S_password,$db);// you can select db separately as you did already
+$conn = mysqli_connect($host,$S_user,$S_password,$db);
+
 if($conn){}
-    
-    
     else{echo "db connection error because of".mysqli_connect_error();}
 
     try{
+            $sql = "SELECT * FROM menu_details WHERE Item_Availability=1 ";
+            $result = mysqli_query($conn, $sql);
 
-        $sql = "SELECT * FROM menu_details WHERE Item_Availability=1 ";
+            echo "Available
+            <div><table>";
+            while($row = $result->fetch_assoc()) {
+                $image = $row['Img_Link'];
+                echo "<tr><td>" . htmlspecialchars($row['Item_Name']) . "</td><td>" . "£" . htmlspecialchars($row['Item_Cost']) . "</td><td>" . '<img src="images/' . $image . '" width="100" height="100"' .  "</td></tr>";
+                }
+            echo "</table></div><br>" ;
 
-        $result = mysqli_query($conn, $sql);
 
-
-        echo "<table>";
-        while($row = $result->fetch_assoc()) {
-            $image = $row['Img_Link'];
-            echo "<tr><td>" . htmlspecialchars($row['Item_Name']) . "</td><td>" . "£" . htmlspecialchars($row['Item_Cost']) . "</td><td>" . '<img src="images/' . $image . '" width="100" height="100"' .  "</td></tr>";
-            }
+            $sql = "SELECT * FROM menu_details WHERE Item_Availability=0 ";
+            $result = mysqli_query($conn, $sql);
+            echo "<br>Un-Available
+            <div><table>";
+            while($row = $result->fetch_assoc()) {
+                $image = $row['Img_Link'];
+                echo "<tr><td>" . htmlspecialchars($row['Item_Name']) . "</td><td>" . "£" . htmlspecialchars($row['Item_Cost']) . "</td><td>" . '<img src="images/' . $image . '" width="150" height="100"' .  "</td></tr>";
+                }
+            echo "</table></div>" ;
         }
-        //url(images/menu-1.jpg)
 
-
-    
     catch(Exception $e) {
         echo 'Incorrect Email or password';
-
 }
 
 ?>
