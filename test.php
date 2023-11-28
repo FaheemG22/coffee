1b6a042949d45aa15f3a7701350648b067c54c95
FaheemG22 <119604215+FaheemG22@users.noreply.github.com>
2023-11-28 22:36:28 +0000

    Styling 2

--- test.php
+++ test.php
@@ -37,7 +37,7 @@
       </div>
 
       <div class="slider-item" style="background-image: url(images/bg_3.jpg);">
-      	<div class="overlay"></div>
+      	<div class="overlay" style="filter: blur(8px);-webkit-filter: blur(8px);"></div>
         <div class="container">
           <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
 
@@ -53,41 +53,54 @@
       </div>
 </section>
     
-<section class="ftco-section" style="background-color:#ffd299;">
-    <div class="container ftco-animate ">
-    <div class="modal" style="display:block;position:static;background-color: rgba(0, 0, 0, 0.00);">
-    <div class="modal-content border rounded-3" style="width:100%;min-width:250px;min-height:500px;">
-            <div class="container">
-            <div class="heading-section ftco-animate ">
-              <div>
-              <span class="subheading text-center">Temp Cake Name</span>
+<section class="ftco-section" style="background-image:url(images/bg_6.jpg);">
+<!--
+  filter: blur(8px);-webkit-filter: blur(8px);
+-->
+    <div class="container ftco-animate">
+      <div class="modal" style="display:block;position:static;background-color: rgba(0, 0, 0, 0.00);">
+        <div class="modal-content border rounded-3" style="width:100%;min-width:250px;min-height:500px;">
+          <div class="container">
+              <div class="heading-section ftco-animate ">
+              
+                <div>
+                <span class="subheading text-center">Temp Cake Name</span>
+                </div>
+                
+                <div>
+                <span class="subheading text-center" style="font-size:35px;margin-top:20px;">2023-11-28 14:56:09</span>
+                </div>
               </div>
-              <div>
-              <span class="subheading text-center" style="font-size:35px;margin-top:20px;">2023-11-28 14:56:09</span>
-              </div>
-            </div>
           
-            <br>
-            <hr>
+              <hr>
 
-            <img src="images/about.jpg" alt="your image" class="img-thumbnail rounded mx-auto d-block">
-            <br>
-            <hr>
-            <div class="container">
-            <div class="heading-section ftco-animate ">
-              <div>
-              <link href="test2.css" rel="stylesheet">
+              <img src="images/about.jpg" class="img-thumbnail rounded mx-auto d-block" style="height:min-content;">
+            
+              <hr>
+              <div class="ftco-animate d-flex justify-content-center" style="margin-top:-40px;">
+                <div class="heart"></div>
+              </div>
 
-              <button type="button" class="btn btn-light btn-circle btn-xl">White circular button</button>
+              <div class="ftco-animate d-flex justify-content-center">
+                <p class="text-center" style="border: 2px solid white;border-radius: 15px;margin-top:-25px;height:50px;width:150px;font-size:x-large;"> 100 Likes</p>
+              </div>
 
-            </div>
-            <br>
-
-            </div>            
-            </div>
-            </div>
-
+          </div>            
+        </div>
+      </div>
+    </div>
       </section>
+<!--heart styling and js-->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+<style>
+.heart {width: 100px;height: 100px;background: url("https://cssanimation.rocks/images/posts/steps/heart.png") no-repeat;background-position: 0 0;cursor: pointer;transition: background-position 5s steps(28);transition-duration: 0s;}
+.heart.is-active {transition-duration: 1s;background-position: -2800px 0;}
+</style>
+
+<script>
+$(function () {$(".heart").on("click", function () {$(this).toggleClass("is-active");});});
+</script>
 
 <script>function items(){document.getElementById('new_item_form').style.display='block';invis()}</script><script src="js/jquery.min.js"></script><script src="js/jquery-migrate-3.0.1.min.js"></script><script src="js/popper.min.js"></script><script src="js/bootstrap.min.js"></script><script src="js/jquery.easing.1.3.js"></script><script src="js/jquery.waypoints.min.js"></script><script src="js/jquery.stellar.min.js"></script><script src="js/owl.carousel.min.js"></script><script src="js/jquery.magnific-popup.min.js"></script><script src="js/aos.js"></script><script src="js/jquery.animateNumber.min.js"></script><script src="js/bootstrap-datepicker.js"></script><script src="js/jquery.timepicker.min.js"></script><script src="js/scrollax.min.js"></script><script src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap&v=weekly"></script><script src="js/google-map.js"></script><script src="js/main.js"></script>
 
