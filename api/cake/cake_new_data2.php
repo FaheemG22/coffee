b432d57e0b610c0bcfcc797f2e9b7f64d245c438
FaheemG22 <119604215+FaheemG22@users.noreply.github.com>
2023-12-12 23:55:10 +0000

    Api cake update

---
+++ api/cake/cake_new_data2.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+$host = 'localhost';
+$S_user = 'root';
+$S_password = '';
+$db ='Coffee';
+
+$conn = mysqli_connect($host,$S_user,$S_password,$db);
+if($conn){}
+if (isset($_GET['Last'])){
+  $last = $_GET['Last'];
+  $sql = "SELECT * from cake_details WHERE Cake_ID >= 5 AND Cake_ID <= 10 AND Cake_ID ORDER BY Cake_ID DESC LIMIT 3 ";
+
+
+  $result = mysqli_query($conn, $sql);
+  $array = array();
+
+  while($row = $result->fetch_assoc()) {
+    $array[] = array(
+    'Cake_ID' => $row['Cake_ID'],
+    'User_ID' => $row['User_ID'],
+    'Cake_Name' => $row['Cake_Name'],
+    'Img_Link' => $row['Img_Link'],
+    'Date_Time' => $row['Date_Time']);
+  }
+
+  echo json_encode($array, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+}
+else {echo'broken request';}
+?>
